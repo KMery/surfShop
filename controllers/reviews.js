0a21880427a8351020d2cd6a6a9f54c144513465
KMery <kmeryleiva@gmail.com>
2021-02-28 17:50:30 -0300

    Set up for reviews

---
+++ controllers/reviews.js
@@ -0,0 +1,17 @@
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+module.exports = {
+    //Reviews create
+    async reviewCreate(req, res, next) {
+        
+    },
+    //Reviews update
+    async reviewUpdate(req, res, next) {
+        
+    },
+    //Reviews delete
+    async reviewDelete(req, res, next) {
+        
+    }
+}
