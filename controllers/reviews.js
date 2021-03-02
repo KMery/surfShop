a55e3bb0aa7f445a626e57a429115ba40878172f
KMery <kmeryleiva@gmail.com>
2021-03-02 20:57:17 -0300

    Restrict user from create more than one review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,7 +5,16 @@ module.exports = {
     //Reviews create
     async reviewCreate(req, res, next) {
         //find post by its id
-        let post = await Post.findById(req.params.id);
+        let post = await Post.findById(req.params.id).populate('reviews').exec();
+        //get length of the reviews from the user
+        let haveReviewed = post.reviews.filter(review => {
+            return review.author.equals(req.user._id);
+        }).length;
+        //check if already has reviewed
+        if (haveReviewed) {
+            req.session.error = 'Sorry, you can only create one review per post.';
+            return res.redirect(`/posts/${post.id}`); 
+        }
         //create review
         req.body.review.author = req.user._id;
         let review = await Review.create(req.body.review);
