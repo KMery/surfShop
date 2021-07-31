9d5b4804fbd772b31f8168dc68faa5d26903f551
KMery <kmeryleiva@gmail.com>
2021-07-31 16:21:48 -0300

    cloudinary

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,12 +2,7 @@ const Post = require('../models/post');
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 let mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapBoxToken });
-const cloudinary = require('cloudinary');
-cloudinary.config({
-    cloud_name: 'dqrm0myct',
-    api_key: '823875831589267',
-    api_secret: process.env.CLOUDINARY_SECRET
-});
+const { cloudinary } = require('../cloudinary');
 
 module.exports = {
     //Post index
@@ -32,10 +27,9 @@ module.exports = {
     async postCreate(req, res, next) {
         req.body.post.images = [];
         for (const file of req.files) {
-            let image = await cloudinary.v2.uploader.upload(file.path);
             req.body.post.images.push({
-                url: image.secure_url,
-                public_id: image.public_id
+                url: file.secure_url,
+                public_id: file.public_id
             });
         };
         let response = await geocodingClient
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const multer = require('multer');
-const upload = multer({'dest': 'uploads/'});
+const { cloudinary, storage } = require('../cloudinary');
+const upload = multer({ storage });
 const { asyncErrorHandler } = require('../middleware');
 const { 
   postIndex, 
